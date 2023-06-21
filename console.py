27dca6587d25e15f51146cf3a8c4ac848f727124
dismond5 <omarkhaled201148@gmail.com>
2023-06-21 11:34:23 -0700

    a

--- console.py
+++ console.py
@@ -2,8 +2,13 @@
 """ Console Module """
 import cmd
 import sys
+from datetime import datetime
+import re
+import os
+import uuid
+
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
